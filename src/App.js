c11148cee9477269289ed580574f80cc5a445288
Axel Salazar <axel_so@msn.com>
2020-04-05 22:27:38 -0500

    Add the context API and provider state

--- src/App.js
+++ src/App.js
@@ -2,18 +2,23 @@ import React, {Component} from 'react';
 import Header from './components/Header';
 import Debtors from './components/Debtors';
 
+import { Provider } from './context';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-				<Header branding="Debtor Manager" />
-				<div className="container">
-          <Debtors />
-				</div>
-      </div>
+      <Provider>
+        <div className="App">
+          <Header branding="Debtor Manager" />
+          <div className="container">
+            <Debtors />
+          </div>
+        </div>
+      </Provider>
+      
     );
   }
 }
--- src/components/Debtors.js
+++ src/components/Debtors.js
@@ -1,54 +1,38 @@
 import React, {Component} from "react";
 import Debtor from './Debtor';
-
+import { Consumer } from '../context';
 
 class Debtors extends Component {
-  state = {
-    debtors: [
-      {
-        id: 1,
-        name: 'John Doe',
-        email: 'jdoe@gmail.com',
-        phone: '555-555-5555'
-      },
-      {
-        id: 2,
-        name: 'Karen Smith',
-        email: 'karen@gmail.com',
-        phone: '333-333-3333'
-      },
-      {
-        id: 3,
-        name: 'Alice Doe',
-        email: 'adoe@gmail.com',
-        phone: '555-555-5555'
-      }
-    ]
-  }
-
+  
   deleteDebtor = id => {
     const { debtors } = this.state;
     const newDebtors = debtors.filter(debtor => debtor.id !== id);
     this.setState({
       debtors: newDebtors,
     })
-    console.log(newDebtors);
   }
 
 	render() {
-    const { debtors } = this.state;
+    return (
+      <Consumer>
+        {value => {
+          const { debtors } = value;
+          return (
+            <React.Fragment>
+              {debtors.map(debtor => (
+                <Debtor 
+                  key={debtor.id}
+                  debtor={debtor} 
+                  deleteClickHandler={this.deleteDebtor.bind(this, debtor.id)}
+                />
+              ))}		
+            </React.Fragment>       
+          )
+        }}
+      </Consumer>
+    )
 
-		return (
-			<React.Fragment>
-		    {debtors.map(debtor => (
-          <Debtor 
-            key={debtor.id}
-            debtor={debtor} 
-            deleteClickHandler={this.deleteDebtor.bind(this, debtor.id)}
-          />
-        ))}		
-			</React.Fragment>
-		);
+
 	}
 }
 
