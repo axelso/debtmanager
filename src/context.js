c4f9fff9b2cd6e864ffa1ca7ea058e4c9777c65a
Axel Salazar <axel_so@msn.com>
2020-04-11 13:42:01 -0500

    Add http get method

--- src/context.js
+++ src/context.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 
 const Context = React.createContext();
 
@@ -21,29 +22,16 @@ const reducer = (state, action) => {
 
 export class Provider extends Component {
   state = {
-    debtors: [
-      {
-        id: 1,
-        name: 'John Doe',
-        email: 'jdoe@gmail.com',
-        phone: '555-555-5555'
-      },
-      {
-        id: 2,
-        name: 'Karen Smith',
-        email: 'karen@gmail.com',
-        phone: '333-333-3333'
-      },
-      {
-        id: 3,
-        name: 'Alice Doe',
-        email: 'adoe@gmail.com',
-        phone: '555-555-5555'
-      }
-    ],
+    debtors: [],
     dispatch: action => this.setState(state => reducer(state, action))
   }
   
+  componentDidMount() {
+    axios.get("https://jsonplaceholder.typicode.com/users")
+      .then(response => this.setState({
+        debtors: response.data
+      }));
+  }
   render() {
     return (
       <Context.Provider value={this.state}>
