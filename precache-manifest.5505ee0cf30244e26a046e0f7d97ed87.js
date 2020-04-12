53ead18f997b4fa5b7994deff240556767ee4bfb
axelso <axel_so@msn.com>
2020-04-11 19:08:38 -0500

    Updates

---
+++ precache-manifest.5505ee0cf30244e26a046e0f7d97ed87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a84f346cbe4a384b87d406dd30e810a6",
+    "url": "/debtmanager/index.html"
+  },
+  {
+    "revision": "15027d857383580d6e1e",
+    "url": "/debtmanager/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "37c716125b8a4682a439",
+    "url": "/debtmanager/static/css/main.65938976.chunk.css"
+  },
+  {
+    "revision": "15027d857383580d6e1e",
+    "url": "/debtmanager/static/js/2.85840541.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/debtmanager/static/js/2.85840541.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37c716125b8a4682a439",
+    "url": "/debtmanager/static/js/main.fe89ddc8.chunk.js"
+  },
+  {
+    "revision": "50b7762224ba9e1e3cab",
+    "url": "/debtmanager/static/js/runtime-main.a5778876.js"
+  }
+]);
